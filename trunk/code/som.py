31348012e19be5c5c620964ef3ed0e432ae0cb3b
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2007-02-22 07:44:57 +0000

    Abstrated topology

--- trunk/code/som.py
+++ trunk/code/som.py
@@ -1,6 +1,11 @@
-import random,math,time,sys
+import random,math,time,sys,os
 from Numeric import *
 from math import acos,sqrt,pi,degrees,sin,cos,asin
+
+
+
+			
+
 
 class som:
 	def __init__(self):
@@ -9,48 +14,148 @@ class som:
 		self.tSteps = 0
 		self.maxN = 0
 		self.alpha0 = 0
-		self.grid = []
 		self.nodes = []
-		# The Cache cache only works if the Neighborhood size is decreasing!
-		# If you increase the maxN clear the cache!
-		self.neighborhoodCache = {}
+		#self.topo = topo
+
 	def load(self,path,name):
+
 		dataf = open(path+name+'.txt','r')
-		try:
-			geof = open(path+name+'_geo.txt','r')
-			geof.next()
-		except:
-			geof = False
 		header = dataf.next()
 		Dims,Size = header.split()
 		self.Dims = int(Dims)
 		self.Size = int(Size)
 		self.nodes = array([[0.0 for i in xrange(self.Dims)] for j in xrange(self.Size)])
-		self.grid = array([[0.0 for i in xrange(2)] for j in xrange(self.Size)])
 
 		for i in xrange(self.Size):
-			if geof:
-				geo = geof.next()
-				geo = geo.split()
-				geo = (math.radians(float(geo[0])),math.radians(float(geo[1])))
-				self.grid[i] = geo
 			
 			data = dataf.next()
 			data = data.split()
 			data = map(float,data)
 			self.nodes[i] = data
+		#Load the topology
+		#self.topo.load(path,name)
 
 	def save(self,path,name):
 		outf = open(path+name+'.txt','w')
-		geof = open(path+name+'_geo.txt','w')
 		outf.write("%d %d\n"%(self.Dims,self.Size))
-		geof.write("%d %d\n"%(self.Dims,self.Size))
 		for i in xrange(self.Size):
 			outf.write(' '.join(str(self.nodes[i].tolist())[1:-1].split(', '))+'\n')
-			geof.write("%f %f\n"%(degrees(self.grid[i][0]),degrees(self.grid[i][1])))
 		outf.close()
-		geof.close()
+		#Save the topology
+		#self.topo.save(path,name)
+
 	def randInit(self):
+	        self.nodes = array([[random.random() for j in xrange(self.Dims)] for i in xrange(self.Size)])
+		#Generate topology
+		#self.topo.randInit()
+
+
+	def findBMU(self,ind,v,ReturnDist = False):
+	        minDist = self.diff(0,ind,v)
+	        BMU = 0 
+	        for i in xrange(1,self.Size):
+	                d = self.diff(i,ind,v)
+	                if d < minDist:
+	                        minDist = d
+	                        BMU = i 
+		if ReturnDist:
+			return BMU,minDist
+		else:
+	        	return BMU
+
+	def diff(self,nodeid,ind,v):
+		node = take(self.nodes[nodeid],ind)	
+		return sum((node-v)**2)
+
+	def alpha(self,t):
+		r = self.alpha0 * (1 - (t/float(self.tSteps)))
+		if r < 0: r = 0
+		return r
+	def hci(self, t, dist):
+		sigma = self.kernalWidth(t)
+		a = self.alpha(t)
+		top = dist**2
+		bottom = (2*(float(sigma)**2))
+		return a * math.exp(-top/bottom)
+	def kernalWidth(self,t):
+		r = round((self.Size*self.maxN) * (1 - (t/float(self.tSteps))))
+		r = int(r)
+		if r == 0: r = 1
+		return r
+
+	def merge(self,t,ind,v):
+		bmu = self.findBMU(ind,v)
+		sigma = self.kernalWidth(t)
+		results = self.neighborhood( bmu , sigma )
+		alteredNodes = [(results[i],self.hci(t,i)) for i in xrange(len(results))]
+		for nodeID,hc in alteredNodes:
+			part = take(self.nodes[nodeID],ind)
+			delta = part+hc*(v-part)
+			put(self.nodes[nodeID],ind,delta)
+
+	def run(self,obsf):
+		self.neighborhoodCache = {}
+		print "####################################"
+		print "###        Configuration         ###"
+		print "####################################"
+		print "  N = %d, maxN = %f"%(self.Size,self.maxN)
+		print "  Total runs: %d"%self.tSteps
+		print "  initial learning rate = %f"%self.alpha0
+		print "  Obs File:%s"%obsf.filename
+		print "####################################"
+		print "###          End Config          ###"
+		print "####################################"
+		print ""
+		print "Running..."
+		t1 = time.time()
+		for t in xrange(self.tSteps):
+			id,ind,v = obsf.stream()
+			self.merge(t,ind,v)
+		print "\nRun compleated in %f seconds"%(time.time()-t1)
+
+	def map(self,obsf,outFileName):
+		outf = file(outFileName,'w')
+		pts = []
+		qerror = 0
+		counter = 0 
+		for id,obs in obsf:
+			bm,err = self.findBMU(obs,ReturnDist=True)
+			qerror += err
+			pt = self.grid[bm]
+			outf.write("%d %f %f %f\n"%(bm,degrees(pt[0]),degrees(pt[1]),err))
+			sys.stdout.write(".%d,%f.\r"%(counter,err))
+			sys.stdout.flush()
+			counter += 1
+		qerror = qerror/counter
+		outf.close()
+		return qerror
+
+
+
+class Topology(som):
+	"""Template class for topology"""
+	def __init__(self):
+		som.__init__(self)
+	def save(self,path,name):
+		som.save(self,path,name)
+	def load(self,path,name):
+		som.load(self,path,name)
+	def randInit(self):
+		som.randInit(self)
+	def neighborhood(self,bmu,NumNeighbors):
+		pass
+		
+	
+class Sphere(som):
+	def __init__(self):
+		som.__init__(self)
+		# The Cache cache only works if the Neighborhood size is decreasing!
+		# If you increase the maxN clear the cache!
+		self.neighborhoodCache = {}
+	def clearCache(self):
+		self.neighborhoodCache = {}
+	def randInit(self):
+		som.randInit(self)
 		N = self.Size
 	        points = []
 	        for i in xrange(1,N+1):
@@ -65,8 +170,29 @@ class som:
 	                points.append((phi,theta))
 	        points = array([(phi-pi,theta-(pi/2)) for phi,theta in points])
 		self.grid = points
-	        self.nodes = array([[random.random() for j in xrange(self.Dims)] for i in xrange(self.Size)])
-
+	def save(self,path,name):
+		som.save(self,path,name)
+		geof = open(path+name+'_geo.txt','w')
+		geof.write("%d %d\n"%(self.Dims,self.Size))
+		for i in xrange(self.Size):
+			geof.write("%f %f\n"%(degrees(self.grid[i][0]),degrees(self.grid[i][1])))
+		geof.close()
+	def load(self,path,name):
+		som.load(self,path,name)
+		if os.path.exists(path+name+'_geo.txt'):
+			geof = open(path+name+'_geo.txt','r')
+			header = geof.next()
+			#Dims,Size = header.split()
+			#self.Dims = int(Dims)
+			#self.Size = int(Size)
+			self.grid = zeros((self.Size,2),typecode='f')
+			for i in xrange(self.Size):
+				geo = geof.next()
+				geo = geo.split()
+				geo = (math.radians(float(geo[0])),math.radians(float(geo[1])))
+				self.grid[i] = geo
+		else:
+			pass
 	def sdist(self,pt1,pt2):
 		phi1,theta1 = pt1
 		phi2,theta2 = pt2
@@ -89,118 +215,9 @@ class som:
 			self.neighborhoodCache[bmu] = pts
 			return pts
 
-	def findBMU(self,ind,v,ReturnDist = False):
-	        minDist = self.diff(0,ind,v)
-	        BMU = 0 
-	        for i in xrange(1,self.Size):
-	                d = self.diff(i,ind,v)
-	                if d < minDist:
-	                        minDist = d
-	                        BMU = i 
-		if ReturnDist:
-			return BMU,minDist
-		else:
-	        	return BMU
-#	def MatchAll(self,obs,ReturnDist = False):
-#		bmList = [(0,0) for i in xrange(self.Size)]
-#		for i in xrange(self.Size):
-#			d = self.diff(self.nodes[i],obs)
-#			bmList[i] = (d,i)
-#		bmList.sort()
-#		return bmList
-
-	def diff(self,nodeid,ind,v):
-		node = take(self.nodes[nodeid],ind)	
-		return sum((node-v)**2)
-#	def diff(self,node,dictObs):
-#	        diff = 0
-#	        for key in dictObs:
-#	                diff += (node[key]-dictObs[key])**2
-#	        #return math.sqrt(diff)
-#	        return diff
-
-	def alpha(self,t):
-		r = self.alpha0 * (1 - (t/float(self.tSteps)))
-		if r < 0: r = 0
-		return r
-	def hci(self, t, dist):
-		sigma = self.kernalWidth(t)
-		a = self.alpha(t)
-		top = dist**2
-		bottom = (2*(float(sigma)**2))
-		return a * math.exp(-top/bottom)
-	def kernalWidth(self,t):
-		r = round((self.Size*self.maxN) * (1 - (t/float(self.tSteps))))
-		r = int(r)
-		if r == 0: r = 1
-		return r
-
-	def merge(self,t,ind,v):
-#		t1 = time.time()
-		bmu = self.findBMU(ind,v)
-#		print "\tFound BMU: %d in %f seconds"%(bmu,time.time()-t1)
-#		t2 = time.time()
-		sigma = self.kernalWidth(t)
-		results = self.neighborhood( bmu , sigma )
-#		print "\tFound neighborhood in %f seconds"%(time.time()-t2)
-#		t2 = time.time()
-		## Nodes = (nodeID, hci)
-		alteredNodes = [(results[i],self.hci(t,i)) for i in xrange(len(results))]
-		for nodeID,hc in alteredNodes:
-			part = take(self.nodes[nodeID],ind)
-			delta = part+hc*(v-part)
-			put(self.nodes[nodeID],ind,delta)
-#		print "\tUpdated %d neighbors in %f seconds"%(sigma,time.time()-t2)
-#		print "\tt in %f seconds"%(time.time()-t1)
-
-	def run(self,obsf):
-		self.neighborhoodCache = {}
-		print "####################################"
-		print "###        Configuration         ###"
-		print "####################################"
-		print "  N = %d, maxN = %f"%(self.Size,self.maxN)
-		print "  Total runs: %d"%self.tSteps
-		print "  initial learning rate = %f"%self.alpha0
-		print "  Obs File:%s"%obsf.filename
-		print "####################################"
-		print "###          End Config          ###"
-		print "####################################"
-		print ""
-		print "Running..."
-		t1 = time.time()
-		for t in xrange(self.tSteps):
-			id,ind,v = obsf.stream()
-			self.merge(t,ind,v)
-		print "\nRun compleated in %f seconds"%(time.time()-t1)
-
-#	def bmap(self,obsf,outFileName):
-#		o = file(outFileName,'w')
-#		o.write("ID,BMU\n")
-#		for id,obs in obsf:
-#			try:
-#				bm = self.findBMU(obs)
-#			except:
-#				print id
-#			o.write("%s,%d\n"%(id,bm))
-#		o.close()
 
 
-	def map(self,obsf,outFileName):
-		outf = file(outFileName,'w')
-		pts = []
-		qerror = 0
-		counter = 0 
-		for id,obs in obsf:
-			bm,err = self.findBMU(obs,ReturnDist=True)
-			qerror += err
-			pt = self.grid[bm]
-			outf.write("%d %f %f %f\n"%(bm,degrees(pt[0]),degrees(pt[1]),err))
-			sys.stdout.write(".%d,%f.\r"%(counter,err))
-			sys.stdout.flush()
-			counter += 1
-		qerror = qerror/counter
-		outf.close()
-		return qerror
+
 class ObsFile:
 	def __init__(self,filename,fileType = 'complete'):
 		self.filename = filename
@@ -226,20 +243,20 @@ class ObsFile:
 		line = self.fileObj.next()
 		line = line.split()
 		id = self.nextLine
-		indices = empty(self.Dims,typecode='s') #typecode 's' is Int16
-		values = empty(self.Dims,typecode='f') #typecode 'f' is Float32
 		for n in xrange(0,self.Dims):
-			indices[n] = n
-			values[n] = float(line[n])
+			self.indices[n] = n
+			self.values[n] = float(line[n])
 		#obs = dict([(n,float(line[n])) for n in xrange(self.Dims)])
 		self.nextLine+=1
-		return id,indices,values
+		return id,self.indices,self.values
 	def reset(self):
 		self.fileObj.seek(0)
 		self.nextLine = 0 #Zero Base
 		if self.fileType == 'complete':
 			self.next = self.Cnext
 			self.Dims = int(self.fileObj.next())
+			self.indices = empty(self.Dims,typecode='s') #typecode 's' is Int16
+			self.values = empty(self.Dims,typecode='f') #typecode 'f' is Float32
 			self.nextLine += 1
 		elif self.fileType == 'sparse':
 			self.next = self.Snext
@@ -256,14 +273,13 @@ class ObsFile:
 
 if __name__=="__main__":
 	import sys,time
-        #sys.stdout = open('numeric_log.txt','w',0)
-        s = som()
+        s = Sphere()
         s.Size = 1000
         s.Dims = 10
         s.maxN = 0.5 
         s.tSteps = 10000
         s.alpha0 = 0.5
-        f = ObsFile('testData/10d-10c-no0.dat','complete')
+        f = ObsFile('testData/10d-10c-no0_scaled.dat','complete')
         print "init"
         s.randInit()
         print "save"
