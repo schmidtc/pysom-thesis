09ce88fb53e455cb771b1d31b2f193d78755ce0f
Charles Schmidt <schmidtc@users.noreply.github.com>
2007-04-05 22:52:23 +0000

    new outline

---
+++ proposal/proposal.mm
@@ -0,0 +1,64 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1175810210379" ID="Freemind_Link_741295560" MODIFIED="1175810941163" TEXT="Proposal">
+<node CREATED="1175810442467" FOLDED="true" ID="Freemind_Link_1278409952" MODIFIED="1175810445311" POSITION="left" TEXT="blah">
+<node CREATED="1175810214635" ID="_" MODIFIED="1175810220582" TEXT="He Said She Said"/>
+<node CREATED="1175810221075" ID="Freemind_Link_1026086098" MODIFIED="1175810254325" TEXT="review of stregths and weaknesses"/>
+<node CREATED="1175810255242" ID="Freemind_Link_1167418268" MODIFIED="1175810268429" TEXT="my method and what it addresses"/>
+</node>
+<node CREATED="1175810924369" FOLDED="true" ID="Freemind_Link_1598221280" MODIFIED="1175810926420" POSITION="right" TEXT="Outline">
+<node CREATED="1175810457682" FOLDED="true" ID="Freemind_Link_471965933" MODIFIED="1175810464509" TEXT="Project Summary">
+<node CREATED="1175810464834" ID="Freemind_Link_1250058802" MODIFIED="1175810469429" TEXT="short intro"/>
+<node CREATED="1175810469833" ID="Freemind_Link_1963270671" MODIFIED="1175810482812" TEXT="intellectual merit">
+<node CREATED="1175810485889" ID="Freemind_Link_379196034" MODIFIED="1175810490868" TEXT="short"/>
+</node>
+<node CREATED="1175810492609" ID="Freemind_Link_1083002178" MODIFIED="1175810498196" TEXT="Broader Impact">
+<node CREATED="1175810498625" ID="Freemind_Link_641684591" MODIFIED="1175810500380" TEXT="short"/>
+</node>
+</node>
+<node CREATED="1175810501841" ID="Freemind_Link_504370445" MODIFIED="1175810510444" TEXT="Introduction"/>
+<node CREATED="1175810511984" FOLDED="true" ID="Freemind_Link_1771702031" MODIFIED="1175810519907" TEXT="Research Objectives">
+<node CREATED="1175810535407" FOLDED="true" ID="Freemind_Link_977379404" MODIFIED="1175810539162" TEXT="Bullet Points">
+<node CREATED="1175810559774" ID="Freemind_Link_495896702" MODIFIED="1175810567306" TEXT="each a section in methodology"/>
+</node>
+</node>
+<node CREATED="1175810568670" FOLDED="true" ID="Freemind_Link_1443519308" MODIFIED="1175810577873" TEXT="Background and Justification">
+<node CREATED="1175810583637" ID="Freemind_Link_848898397" MODIFIED="1175810587424" TEXT="he said she said"/>
+<node CREATED="1175810587933" ID="Freemind_Link_317101352" MODIFIED="1175810600536" TEXT="organized"/>
+</node>
+<node CREATED="1175810613732" FOLDED="true" ID="Freemind_Link_1458948765" MODIFIED="1175810617831" TEXT="Research Design">
+<node CREATED="1175810620940" FOLDED="true" ID="Freemind_Link_1585343483" MODIFIED="1175810623103" TEXT="Methods">
+<node CREATED="1175810638659" ID="Freemind_Link_349542301" MODIFIED="1175810661102" TEXT="expand on objectives"/>
+</node>
+<node CREATED="1175810624451" ID="Freemind_Link_1570816703" MODIFIED="1175810625873" TEXT="Data"/>
+</node>
+<node CREATED="1175810667866" ID="Freemind_Link_484904408" MODIFIED="1175810672749" TEXT="Conclusion"/>
+</node>
+<node CREATED="1175810942855" ID="Freemind_Link_984065019" MODIFIED="1175810953154" POSITION="right" TEXT="Research Objectives">
+<node CREATED="1175812589896" ID="Freemind_Link_669847815" MODIFIED="1175812640578" TEXT="Generate synthetic data for use in testing different arragnements of neurons on a sphere"/>
+<node CREATED="1175812705036" ID="Freemind_Link_66698760" MODIFIED="1175813331311" TEXT="Train several SOMs with comparable parameters using different network topologies">
+<node CREATED="1175813349348" ID="Freemind_Link_666114791" MODIFIED="1175813353927" TEXT="Types">
+<node CREATED="1175812753874" ID="Freemind_Link_355201287" MODIFIED="1175812759989" TEXT="Geodesic"/>
+<node CREATED="1175812760602" ID="Freemind_Link_1726313624" MODIFIED="1175812763469" TEXT="Rakmonvon"/>
+<node CREATED="1175812763994" ID="Freemind_Link_679976884" MODIFIED="1175812768869" TEXT="Rectangular"/>
+<node CREATED="1175812769257" ID="Freemind_Link_491151329" MODIFIED="1175812771149" TEXT="Hexagonal"/>
+<node CREATED="1175812902052" ID="Freemind_Link_557785999" MODIFIED="1175812932327" TEXT="Artificailly induced edges"/>
+</node>
+</node>
+<node CREATED="1175813196682" ID="Freemind_Link_923919568" MODIFIED="1175813200429" TEXT="Diagnostics:">
+<node CREATED="1175813017937" ID="Freemind_Link_1424995788" MODIFIED="1175813185669" TEXT="Visualize the reverse mapping of neurons back on to the data"/>
+<node CREATED="1175813217193" ID="Freemind_Link_1771176221" MODIFIED="1175813259850" TEXT="Plot the internal variance of neurons against their first-order neighborhood size">
+<node CREATED="1175813362955" ID="Freemind_Link_1317858192" MODIFIED="1175813432628" TEXT="Generate one plot per trained SOM"/>
+</node>
+</node>
+<node CREATED="1175812497228" ID="Freemind_Link_740736166" MODIFIED="1175812560998" TEXT="Comapre the varience of observations mapped to a neuron for different arrangments of neurons on a sphere">
+<edge WIDTH="thin"/>
+</node>
+<node CREATED="1175811457332" ID="Freemind_Link_814139676" MODIFIED="1175812459937" TEXT="Compare the effects of different irregular neural lattices in Spherical SOM">
+<node CREATED="1175811864524" ID="Freemind_Link_1672031735" MODIFIED="1175811869048" TEXT="plots"/>
+</node>
+<node CREATED="1175810953663" ID="Freemind_Link_234116441" MODIFIED="1175811470430" TEXT="Determine the usefulness of more irregular topologies which offer greater control over network size."/>
+</node>
+<node CREATED="1175812388016" ID="Freemind_Link_1088356969" MODIFIED="1175812391971" POSITION="right" TEXT="new SSOM"/>
+</node>
+</map>
