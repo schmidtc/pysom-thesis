f7b2a0c34ac50b054886ed98baaa138e74150152
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2007-04-07 02:25:04 +0000

    expanded research objects, reworded intro

--- trunk/proposal/proposal.mm
+++ trunk/proposal/proposal.mm
@@ -17,26 +17,52 @@
 </node>
 </node>
 <node CREATED="1175810501841" ID="Freemind_Link_504370445" MODIFIED="1175810510444" TEXT="Introduction"/>
-<node CREATED="1175810511984" FOLDED="true" ID="Freemind_Link_1771702031" MODIFIED="1175810519907" TEXT="Research Objectives">
+<node CREATED="1175810511984" ID="Freemind_Link_1771702031" MODIFIED="1175810519907" TEXT="Research Objectives">
 <node CREATED="1175810535407" FOLDED="true" ID="Freemind_Link_977379404" MODIFIED="1175810539162" TEXT="Bullet Points">
 <node CREATED="1175810559774" ID="Freemind_Link_495896702" MODIFIED="1175810567306" TEXT="each a section in methodology"/>
 </node>
 </node>
-<node CREATED="1175810568670" FOLDED="true" ID="Freemind_Link_1443519308" MODIFIED="1175810577873" TEXT="Background and Justification">
+<node CREATED="1175810568670" ID="Freemind_Link_1443519308" MODIFIED="1175810577873" TEXT="Background and Justification">
 <node CREATED="1175810583637" ID="Freemind_Link_848898397" MODIFIED="1175810587424" TEXT="he said she said"/>
 <node CREATED="1175810587933" ID="Freemind_Link_317101352" MODIFIED="1175810600536" TEXT="organized"/>
 </node>
-<node CREATED="1175810613732" FOLDED="true" ID="Freemind_Link_1458948765" MODIFIED="1175810617831" TEXT="Research Design">
-<node CREATED="1175810620940" FOLDED="true" ID="Freemind_Link_1585343483" MODIFIED="1175810623103" TEXT="Methods">
+<node CREATED="1175810613732" ID="Freemind_Link_1458948765" MODIFIED="1175810617831" TEXT="Research Design">
+<node CREATED="1175810620940" ID="Freemind_Link_1585343483" MODIFIED="1175810623103" TEXT="Methods">
 <node CREATED="1175810638659" ID="Freemind_Link_349542301" MODIFIED="1175810661102" TEXT="expand on objectives"/>
 </node>
 <node CREATED="1175810624451" ID="Freemind_Link_1570816703" MODIFIED="1175810625873" TEXT="Data"/>
 </node>
 <node CREATED="1175810667866" ID="Freemind_Link_484904408" MODIFIED="1175810672749" TEXT="Conclusion"/>
 </node>
+<node CREATED="1175902733551" FOLDED="true" ID="Freemind_Link_1465106448" MODIFIED="1175902736819" POSITION="right" TEXT="Introduction">
+<node CREATED="1175902737151" ID="Freemind_Link_131842068" MODIFIED="1175902745667" TEXT="Research Statement">
+<node CREATED="1175902746143" ID="Freemind_Link_1426846375" MODIFIED="1175902992601" TEXT="Highly regular spherical network topologies are gererally considered acceptible for reducing the edge effect.  However, slightly more irregular spherical topologies, which offer greater control over network size, may also be acceptible."/>
+</node>
+<node CREATED="1175903094929" ID="Freemind_Link_1333115547" MODIFIED="1175906189802" TEXT="Using a spherical lattice is widely suggested as a solution to the boundary&#xa;effect found in the traditional Self-Organizing Map (SOM) \citep{ritter99,&#xa;boudjemai2003, sangole03, wu2006, Nishio:2006fk}.  However, the use of the&#xa;spherical lattice introduces a new problem.  With the exception of the five platonic solids,&#xa;distributing points on a sphere will always result in irregular topology&#xa;\citep{ritter99}.  The classic method for reducing this irregularity is to generate&#xa;the spherical lattice by tessellating the sides of the icosahedron \citep{Niskio:2006fk}.&#xa;While this method will always result in a highly regular spherical topology, the&#xa;main drawback is that the network size, \(N\), grows exponentially. Other&#xa;methods for arranging neurons on the sphere allow for unlimited control over&#xa;network size, but yield topologies with increased irregularity&#xa;\citep{harris2000,wu2005,Niskio:2006fh}.  The goal of this research is to&#xa;investigate the extent to which irregularity in the spherical topology effects&#xa;the training of the SOM, in order to determine the suitability of irregular&#xa;topologies for use in SOM."/>
+</node>
 <node CREATED="1175810942855" ID="Freemind_Link_984065019" MODIFIED="1175810953154" POSITION="right" TEXT="Research Objectives">
+<node CREATED="1175810953663" ID="Freemind_Link_234116441" MODIFIED="1175811470430" TEXT="Determine the usefulness of more irregular topologies which offer greater control over network size."/>
+<node CREATED="1175813196682" ID="Freemind_Link_923919568" MODIFIED="1175906334502" TEXT="Develop Diagnostics:">
+<node CREATED="1175813017937" ID="Freemind_Link_1424995788" MODIFIED="1175813185669" TEXT="Visualize the reverse mapping of neurons back on to the data">
+<node CREATED="1175901974285" ID="Freemind_Link_174259458" MODIFIED="1175901986134" TEXT="4 maps (one per topology)"/>
+<node CREATED="1175902574374" ID="Freemind_Link_1895138258" MODIFIED="1175902584594" TEXT="QE(node to MEAN INPUT)"/>
+<node CREATED="1175902584982" ID="Freemind_Link_1245290973" MODIFIED="1175902595619" TEXT="QE(note to Best Matching INPUT)"/>
+</node>
+<node CREATED="1175813217193" FOLDED="true" ID="Freemind_Link_1771176221" MODIFIED="1175902390937" TEXT="Plot the internal variance of each neuron against its first-order neighborhood size">
+<node CREATED="1175813362955" ID="Freemind_Link_1317858192" MODIFIED="1175813432628" TEXT="Generate one plot per trained SOM"/>
+<node CREATED="1175902499278" ID="Freemind_Link_733187859" MODIFIED="1175902510545" TEXT="X = per node 1st order neighborhood size"/>
+<node CREATED="1175902481646" ID="Freemind_Link_1726088111" MODIFIED="1175902498353" TEXT="Y = per node varience"/>
+</node>
+<node CREATED="1175902154004" FOLDED="true" ID="Freemind_Link_28488148" MODIFIED="1175902444625" TEXT="Plot the internal variance of each neuron against the variance in neighborhood size for each topology">
+<edge WIDTH="thin"/>
+<node CREATED="1175902192108" ID="Freemind_Link_1912357353" MODIFIED="1175902203607" TEXT="One plot contains all trained SOMs"/>
+<node CREATED="1175902514630" ID="Freemind_Link_1383870796" MODIFIED="1175902533882" TEXT="X = per SOM variance in 1st order neighborhood size"/>
+<node CREATED="1175902534318" ID="Freemind_Link_1977389720" MODIFIED="1175902540794" TEXT="Y = per node varience"/>
+</node>
+</node>
+<node CREATED="1175906400259" ID="Freemind_Link_903889245" MODIFIED="1175906411911" TEXT="Test the Diagnostics using Sythetic Data">
 <node CREATED="1175812589896" ID="Freemind_Link_669847815" MODIFIED="1175812640578" TEXT="Generate synthetic data for use in testing different arragnements of neurons on a sphere"/>
-<node CREATED="1175812705036" ID="Freemind_Link_66698760" MODIFIED="1175813331311" TEXT="Train several SOMs with comparable parameters using different network topologies">
+<node CREATED="1175812705036" ID="Freemind_Link_66698760" MODIFIED="1175813331311" TEXT="Train several SOMs with comparable parameters using different network topologies"/>
 <node CREATED="1175813349348" ID="Freemind_Link_666114791" MODIFIED="1175813353927" TEXT="Types">
 <node CREATED="1175812753874" ID="Freemind_Link_355201287" MODIFIED="1175812759989" TEXT="Geodesic"/>
 <node CREATED="1175812760602" ID="Freemind_Link_1726313624" MODIFIED="1175812763469" TEXT="Rakmonvon"/>
@@ -45,19 +71,24 @@
 <node CREATED="1175812902052" ID="Freemind_Link_557785999" MODIFIED="1175812932327" TEXT="Artificailly induced edges"/>
 </node>
 </node>
-<node CREATED="1175813196682" ID="Freemind_Link_923919568" MODIFIED="1175813200429" TEXT="Diagnostics:">
-<node CREATED="1175813017937" ID="Freemind_Link_1424995788" MODIFIED="1175813185669" TEXT="Visualize the reverse mapping of neurons back on to the data"/>
-<node CREATED="1175813217193" ID="Freemind_Link_1771176221" MODIFIED="1175813259850" TEXT="Plot the internal variance of neurons against their first-order neighborhood size">
-<node CREATED="1175813362955" ID="Freemind_Link_1317858192" MODIFIED="1175813432628" TEXT="Generate one plot per trained SOM"/>
 </node>
-</node>
-<node CREATED="1175812497228" ID="Freemind_Link_740736166" MODIFIED="1175812560998" TEXT="Comapre the varience of observations mapped to a neuron for different arrangments of neurons on a sphere">
-<edge WIDTH="thin"/>
-</node>
-<node CREATED="1175811457332" ID="Freemind_Link_814139676" MODIFIED="1175812459937" TEXT="Compare the effects of different irregular neural lattices in Spherical SOM">
-<node CREATED="1175811864524" ID="Freemind_Link_1672031735" MODIFIED="1175811869048" TEXT="plots"/>
-</node>
-<node CREATED="1175810953663" ID="Freemind_Link_234116441" MODIFIED="1175811470430" TEXT="Determine the usefulness of more irregular topologies which offer greater control over network size."/>
+<node CREATED="1175810568670" FOLDED="true" ID="Freemind_Link_1068372893" MODIFIED="1175810577873" POSITION="right" TEXT="Background and Justification">
+<node CREATED="1175907609629" MODIFIED="1175907609629" TEXT="The Self-Organizing Map (SOM) is an unsupervised competitive learning process developed by Teuvo Kohonen as a technique to analyze high dimensional data sets.  The SOM algorithm uses an artificial neural network to organize high dimensional data onto a low dimensional lattice, or map, of neurons.  Each neuron contains a reference vector that can be considered to model a portion of the input space. Before training these neurons are initialized, most commonly to random values.  During the training process a randomly selected input vector searches the map for its best matching unit (BMU), that is the neuron to which it is most similar. The BMU and its neighborhood, as defined by a neighborhood function, are than adjusted to better match that observation \citep{Kohonen2000}.  The training process is repeated a predefined number of times, or ideally until the map converges.  The traditional SOM is laid out on a two dimensional plane using either a rectangular or hexagonal topology.  According to \cite{wu2006} the hexagonal structure is more uniform and generally preferred."/>
+<node CREATED="1175907609638" MODIFIED="1175907609638" TEXT="%\subsection{Boundary Effect}"/>
+<node CREATED="1175907609638" MODIFIED="1175907609638" TEXT="One obvious drawback of building the neural lattice in a discrete euclidean plane is the boundary of the resulting lattice.  A neuron located on the boundry has fewer neighbors and thus fewer chances of being updated \citep{wu2006}.  As observed in Figure \ref{figure1}, neurons in the center of the map tend to better represent the mean of the input-space.  This is arguably caused by outliers being pushed to the edges of the map, where they encounter fewer competing signals."/>
+<node CREATED="1175907645392" MODIFIED="1175907645392" TEXT="\subsection{Spherical SOM}"/>
+<node CREATED="1175907645392" MODIFIED="1175907645392" TEXT="A natural way to eliminate the boundary effect is to wrap the lattice around a three dimensional object such as a sphere or torus, therefor removing the edge entirely. The toroidal SOM was introduced by \cite{li1993}, however the torus is not hugely effective for visualization, as maps generated from a torus are not very intuitive \citep{ito2000,wu2006}.  \cite{ritter99} describes the torus as being topologically flat and suggests that a curved topology, such as that of a sphere, may better reflect directional data.  A sphere also results in a more intuitive map, since we are accustomed to looking at maps based on a sphere."/>
+<node CREATED="1175907645395" MODIFIED="1175907645395" TEXT="\cite{ritter99} first introduced the spherical SOM and several enhancements have since been suggested \citep{boudjemai2003,sangole03,Nishio:2006fk,wu2006}.  A good comparison of these enhancements can be found in \citep{wu2006}.  All of these methods derive their spherical structure through the tessellation of a polyhedron as originally proposed by \citeauthor{ritter99}.  \cite{wu2006} point out the importance of a uniform distribution on the sphere and that it is preferable for all neurons to have an equal number of neighbors and to be equally spaced.  They find generally that the tessellation method best satisfies these conditions and specifically that the icosahedron is the best starting point \citep{wu2005}. Tessellation of the icosahedron results in a network of neurons, each of which have exactly six neighbors, save the original twelve which each have five.  This is very close to the ideal structure in which every neuron would have exactly six neighbors.  This structure has very low variances in both neuron spacing and neighbor size."/>
+<node CREATED="1175907645399" MODIFIED="1175907645399" TEXT="\subsection{Network Size}"/>
+<node CREATED="1175907645400" MODIFIED="1175907645400" TEXT="The cost of releying on \citeauthor{ritter99}&apos;s tesselation method is decreased control over network size. \citeauthor{ritter99}&apos;s tesselation methods results in a rework site that grows at a rate of \(N=2+10*4^f\). \cite{wu2006} offer a slight improvement, rather then recursivly subsidiveding the faces, they redivid the original icosehdron with each step resulting in \(N=2+10*f^2\)."/>
+<node CREATED="1175907645401" MODIFIED="1175907645401" TEXT="The tessellation method used by the class of spherical SOMs based on Ritter&apos;s work results in a network size that is a function of the tessellation frequency and therefore grows exponentially. In practice 2D Euclidean SOMs also offer limited control over network size, as it is undesirable to have one dimension dramatically larger then the other.  \cite{Nishio:2006fk} try to address the issue of network size granularity by departing from the tessellation method and suggesting the use of a partitioned helix to uniformly distribute any number of neurons on a sphere.  A similar method was dismissed by \cite{wu2005} for failing to satisfy the uniformity conditions.  \citeauthor{Nishio:2006fk} seem to have addressed variance in nearest neighbor distances, but the issue of having a uniform number of direct neighbors is still not addressed."/>
+<node CREATED="1175907645404" MODIFIED="1175907645404" TEXT="%Add background material on why Network Size is important.  The need for big and small networks. Waiting for Kohonen Book!!!"/>
+<node CREATED="1175907645404" MODIFIED="1175907645404" TEXT="The literature offers little theoretical guidance on network size \citep{cho1996}.  \cite{toolbox} suggests simply using a network size of \(5*\sqrt {n}\), where \(n\) is the number of observations."/>
+<node CREATED="1175907645405" MODIFIED="1175907645405" TEXT="\subsection{Uniformity}"/>
+<node CREATED="1175907645406" MODIFIED="1175907645406" TEXT="%\subsubsection{Neuron Spacing and Neighborhood Size Variance}"/>
+<node CREATED="1175907645406" MODIFIED="1175907645406" TEXT="\citeauthor{wu2006} state that ``[f]or SOM, it is desirable to have all neurons receive equal geometrical treatment&apos;&apos; \cite[pp. 900]{wu2006}.  To satisfy this constraint two conditions must be met.  Firstly, each neuron should occupy the same amount of space on the given surface.  Secondly, each neuron should be bordered by the same number of surrounding neurons, and we should maximize that number.  The first condition in hugely important for visualization, but irrelevant for training.  During the training of the SOM only the topology of the neurons is considered."/>
+<node CREATED="1175907645408" MODIFIED="1175907645408" TEXT="Based solely on measures of neuron spacing \cite{wu2005} dismissed a method proposed by \cite{Rakhmanov94} for distributing points on a sphere.  Similarly \cite{Nishio:2006fk} use these variance measures to support their helix algorithm for distributing points on a sphere.  Table \ref{table1} shows that these metrics can be misleading and may not be comparable across topologies.  The traditional rectangular and hexagonal topologies have no variance in neuron spacing and the generally preferred hexagonal structure displays greater variance in neighborhood size than the rectangular structure.  The torus by comparison would have variance in neuron spacing, yet no variance in neighborhood size.  The distance between two neurons is only considered during the formation of the neuronal network.  At this stage the spacing is significant as it plays a part in determining neuron adjacency. However using this measure to evaluate potential topologies for use in SOM may be misleading."/>
+<node CREATED="1175907679299" ID="Freemind_Link_1438664248" MODIFIED="1175907680518" TEXT="Methods, for distributing points on the sphere, which allow for fine grained control over network size produce slightly more irregular topologies.  However, no discussion of these irregularities or their effects on SOM training has occurred in the literature. Given that limited theoretical guidance is available for choosing network size the desire for finer control over the network size, should not be overlooked. In particular for a larger SOM the ideal network size may not be achievable via tessellation of the icosahedron. "/>
 </node>
 <node CREATED="1175812388016" ID="Freemind_Link_1088356969" MODIFIED="1175812391971" POSITION="right" TEXT="new SSOM"/>
 <node CREATED="1169584470891" ID="Freemind_Link_989002075" MODIFIED="1169584489807" POSITION="left" TEXT="Sphereical SOM">
