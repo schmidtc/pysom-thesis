202ba87c970d1bf72a7d54d340eafbef754a987b
positrons <positrons@74a62d64-5f28-0410-90a5-c95733db7968>
2007-04-14 06:25:51 +0000

    Added normalization. Expanded example run.

--- trunk/ArcSOM/scripts/mapinit.bat
+++ trunk/ArcSOM/scripts/mapinit.bat
@@ -1,5 +1,1 @@
-Time /T >> Z:\aag\som\init.log
-Date /T >> Z:\aag\som\init.log
-@Z:\svn\SOM_PAK\mapinit.exe -din %1 -cout %2 -topol %3 -neigh %4 -xdim %5 -ydim %6 -init %7 >> Z:\aag\som\init.log
-Time /T >> Z:\aag\som\init.log
-Date /T >> Z:\aag\som\init.log
+@Z:\svn\SOM_PAK\mapinit.exe -din %1 -cout %2 -topol %3 -neigh %4 -xdim %5 -ydim %6 -init %7
