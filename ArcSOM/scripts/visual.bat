c5699bed68d953b0e5a61cc266ce3b19eddc911d
positrons <positrons@gmail.com>
2007-04-14 06:25:51 +0000

    Added normalization. Expanded example run.

--- ArcSOM/scripts/visual.bat
+++ ArcSOM/scripts/visual.bat
@@ -4,36 +4,26 @@
 
 @Echo no optional parameters passed
 @Echo begining visual.exe
-Time /T >> Z:\aag\som\bmu.log
-Date /T >> Z:\aag\som\bmu.log
-@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 >> Z:\aag\som\bmu.log
+@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3
 @goto end
 
 :skip
 @if not %5 == "#" goto skipbuffer
 @Echo vector no skip mode 
 @Echo begining visual.exe
-Time /T >> Z:\aag\som\bmu.log
-Date /T >> Z:\aag\som\bmu.log
-@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -noskip >> Z:\aag\som\bmu.log
+@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -noskip
 @goto end
 
 :buffer
 @Echo read buffer mode
 @Echo begining visual.exe
-Time /T >> Z:\aag\som\bmu.log
-Date /T >> Z:\aag\som\bmu.log
-@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -buffer %5 >> Z:\aag\som\bmu.log
+@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -buffer %5
 @goto end
 
 :skipbuffer
 @Echo vector no skip and read buffer mode
 @Echo begining visual.exe
-Time /T >> Z:\aag\som\bmu.log
-Date /T >> Z:\aag\som\bmu.log
-@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -noskip -buffer %5 >> Z:\aag\som\bmu.log
+@Z:\svn\SOM_PAK\visual.exe -din %1 -cin %2 -dout %3 -noskip -buffer %5
 @goto end
 
 :end 
-Time /T >> Z:\aag\som\bmu.log
-Date /T >> Z:\aag\som\bmu.log
--- ArcSOM/scripts/vsom.bat
+++ ArcSOM/scripts/vsom.bat
@@ -1,5 +1,1 @@
-Time /T >> Z:\aag\som\vsom.log
-Date /T >> Z:\aag\som\vsom.log
-@Z:\svn\SOM_PAK\vsom.exe -din %1 -cin %2 -cout %3 -rlen %4 -alpha %5 -radius %6 >> Z:\aag\som\vsom.log
-Time /T >> Z:\aag\som\vsom.log
-Date /T >> Z:\aag\som\vsom.log
+@Z:\svn\SOM_PAK\vsom.exe -din %1 -cin %2 -cout %3 -rlen %4 -alpha %5 -radius %6
