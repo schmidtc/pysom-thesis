01f756c9c8032db2c88ee8ddadf2e5c416914b2e
positrons <positrons@74a62d64-5f28-0410-90a5-c95733db7968>
2007-04-16 21:05:10 +0000

    Still working on example

--- trunk/ArcSOM/scripts/beta/subset.py
+++ trunk/ArcSOM/scripts/beta/subset.py
@@ -1,5 +1,5 @@
 #input file
-ifile="z:/svn/data/example/bmustate.txt"
+ifile="z:/svn/data/example/BMUdata.txt"
 ofile="z:/svn/data/example/segs/bmustate"
 
 i=open(ifile,'r')
@@ -8,12 +8,24 @@ h=i.readline()
 
 lines=i.readlines()
 i.close()
+
+lines2=[]
+for m in range(49):
+    for n in range(10):
+        l=lines[m+(49*n)]
+        l=l.strip().split(' ')
+        l[2]=str(1900+(n*10))
+        l=' '.join(l)+'\n'        
+        lines2.append(l)
+
+lines=lines2
+
 for m in range(49):
     for n in range (9):
         o=open(ofile+str(m)+'-'+str(n)+'.txt','w')
         o.write(h)
         o.write(lines[m+(49*n)])
-        o.write(lines[m+(49*n)+1])
+        o.write(lines[m+(49*(n+1))])
         o.close()  
       
         
