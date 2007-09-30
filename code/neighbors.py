06e031d239a10d033635e683efb2e0703a07168f
Charles Schmidt <schmidtc@users.noreply.github.com>
2007-09-30 09:18:59 +0000

    can find neighbors by order

--- code/neighbors.py
+++ code/neighbors.py
@@ -1,52 +1,16 @@
-import numpy as N
-import scipy as S
-import scipy.sparse
 import networkx as NX
 
-class graph:
-    def __init__(self,size):
-        self.G = S.sparse.dok_matrix(size)
-        #self.G.setdiag(N.ones(self.G.shape[0]))
-        self.nodes = {}
-        self.ids = {}
-        self.order = 1
-        self.A = self.G
-        self.S = S.sparse.dok_matrix(size)
-        
-    def set_order(self,order):
-        if not self.order == order:
-            self.order = order
-            self.A = self.G
-            for i in range(order-1):
-                self.A = self.A * self.G
-                # Use S to sum non-zero element.
-                
-    def add_node(self,node):
-        if node not in self.nodes:
-            id = len(self.nodes)
-            self.nodes[node] = id
-            self.ids[id] = node
-    def add_edge(self,edge):
-        """edge == tuple('node1','node2')
-            nodes added as necessary"""
-        n1,n2 = edge
-        self.add_node(n1)
-        self.add_node(n2)
-        self.G[self.nodes[n1],self.nodes[n2]]=1
-        self.G[self.nodes[n2],self.nodes[n1]]=1
-    def neighbors(self,node,order=1):
-        self.set_order(order)
-        row = self.A[:,self.nodes[node]]
-        result = []
-        for id,dist in enumerate(row.todense()):
-            if id in self.ids:
-                nodeName = self.ids[id]
-                if nodeName==node:
-                    pass
-                else:
-                    result.append((nodeName,dist))
-        return result
-
+def neighborhood(G,n,o):
+    base = G[n]
+    neighbors = {}
+    neighbors[n] = 0
+    for i in range(1,o+1):
+        for node in neighbors.keys():
+            branch = G[node]
+            for node in branch:
+                if node not in neighbors:
+                    neighbors[node]=i
+    return neighbors
 
 if __name__=='__main__':
 #    G = graph((6,6))
@@ -54,17 +18,6 @@ if __name__=='__main__':
 #        G.add_node(str(i))
     G = NX.Graph()
     
-    def neighborhood(G,n,o):
-        base = G[n]
-        neighbors = {}
-        neighbors[n] = 0
-        for i in range(1,o+1):
-            for node in neighbors.keys():
-                branch = G[node]
-                for node in branch:
-                    if node not in neighbors:
-                        neighbors[node]=i
-        return neighbors
     
 
     G.add_edge(('1','2'))
