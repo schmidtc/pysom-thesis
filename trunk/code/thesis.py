b59960408bde288b0da03f85072311a510078dab
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2007-10-09 23:32:23 +0000

    organize the code

--- trunk/code/thesis.py
+++ trunk/code/thesis.py
@@ -1,5 +1,4 @@
 from som import *
-
 # Research Question 1...
 # Calculat the internal variance for each nueon in the network.
 def pairWiseDist(ids,lines):
@@ -73,76 +72,7 @@ def graphTestMapIV():
     
     return {'sphere':(ivData),'rook':(ivData2)}
 
-### The Training
-### Current, 642, 23*28
-### Future, 4842, 70*69
-def graphTest():
-    G = delaunay.parseDelaunay("delaunay/642_delaunay.xyz")
-    s = GraphTopology(G)
-    s.Dims = 15
-    s.maxN = 0.5
-    s.tSteps = 100000
-    s.alpha0 = 0.04
-    f = ObsFile('testData/15d-40c-no0_scaled.dat','complete')
-    print "init"
-    s.randInit()
-    print "run t=100K"
-    s.run(f)
-    s.save('testResults/','graph_100k')
-    s.maxN = 0.333
-    s.tSteps = 1000000
-    s.alpha0 = 0.03
-    f.reset()
-    print "run t=1M"
-    s.run(f)
-    s.save('testResults/','graph_1m')
-
-    f.reset()
-    s.map(f)
-    s.save('testResults/','graph_1m')
-
-    f.close()
-    return s
-def rookGraphTest():
-    g = grid2Rook(23,28,binary=1)
-    G = NX.Graph()
-    for node in g:
-        for neighbor in g[node][1]:
-            G.add_edge((node,neighbor))
-    s = GraphTopology(G)
-    s.Dims = 15
-    s.maxN = 0.5
-    s.tSteps = 100000
-    s.alpha0 = 0.04
-    f = ObsFile('testData/15d-40c-no0_scaled.dat','complete')
-    print "init"
-    s.randInit()
-    print "run t=100K"
-    s.run(f)
-    s.save('testResults/','rook_100k')
-
-    s.maxN = 0.333
-    s.tSteps = 1000000
-    s.alpha0 = 0.03
-    f.reset()
-    print "run t=1M"
-    s.run(f)
-    s.save('testResults/','rook_1m')
-
-    f.reset()
-    s.map(f)
-    s.save('testResults/','rook_1m')
-
-    f.close()
-    return s
-### End Training
-
 if __name__=="__main__":
-    ### Step one, train the soms
-    #g = graphTest()
-    #r = rookGraphTest()
-    #s = sphereTest() # This is probably no good anymore (or ever)
-
     ### Step two, find internal variance, plot against degree
     #q1 = graphTestMapIV()
     #ivData = q1['sphere']
