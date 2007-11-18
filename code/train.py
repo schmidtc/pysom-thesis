09296a8acaec649579544d91c9d37090f002e655
Charles Schmidt <schmidtc@users.noreply.github.com>
2007-11-18 02:47:39 +0000

    new training

--- code/train.py
+++ code/train.py
@@ -102,65 +102,30 @@ def Test():
     f.close()
     return s
 
-if __name__=="__main__":
-    s = Test()
-    #graphTest(5,0,0)
-    #graphTest(5,2,0)
-    #graphTest(5,10,0)
-if __name__=="__joe__":
-    ### Step one, train the soms
-    graphTest(5,0,0)
-    graphTest(5,0,1)
-    graphTest(10,0,0)
-    graphTest(10,0,1)
-    graphTest(20,0,0)
-    graphTest(20,0,1)
-
-    graphTest(5,2,0)
-    graphTest(5,2,1)
-    graphTest(10,2,0)
-    graphTest(10,2,1)
-    graphTest(20,2,0)
-    graphTest(20,2,1)
-
+def neve():
     graphTest(5,10,0)
     graphTest(5,10,1)
-    graphTest(10,10,0)
-    graphTest(10,10,1)
-    graphTest(20,10,0)
-    graphTest(20,10,1)
-
-    graphTest(5,20,0)
-    graphTest(5,20,1)
-    graphTest(10,20,0)
-    graphTest(10,20,1)
-    graphTest(20,20,0)
-    graphTest(20,20,1)
-
-    rookGraphTest(5,0,0)
-    rookGraphTest(5,0,1)
-    rookGraphTest(10,0,0)
-    rookGraphTest(10,0,1)
-    rookGraphTest(20,0,0)
-    rookGraphTest(20,0,1)
-
-    rookGraphTest(5,2,0)
-    rookGraphTest(5,2,1)
-    rookGraphTest(10,2,0)
-    rookGraphTest(10,2,1)
-    rookGraphTest(20,2,0)
-    rookGraphTest(20,2,1)
-
+    graphTest(5,10,2)
+    graphTest(5,10,3)
+    graphTest(5,10,4)
+    graphTest(5,10,5)
+    graphTest(5,10,6)
+def haar():
     rookGraphTest(5,10,0)
     rookGraphTest(5,10,1)
-    rookGraphTest(10,10,0)
-    rookGraphTest(10,10,1)
-    rookGraphTest(20,10,0)
-    rookGraphTest(20,10,1)
-
-    rookGraphTest(5,20,0)
-    rookGraphTest(5,20,1)
-    rookGraphTest(10,20,0)
-    rookGraphTest(10,20,1)
-    rookGraphTest(20,20,0)
-    rookGraphTest(20,20,1)
+    rookGraphTest(5,10,2)
+    rookGraphTest(5,10,3)
+    rookGraphTest(5,10,4)
+    rookGraphTest(5,10,5)
+def bora():
+    graphTest(5,10,7)
+    graphTest(5,10,8)
+    graphTest(5,10,9)
+    rookGraphTest(5,10,6)
+    rookGraphTest(5,10,7)
+    rookGraphTest(5,10,8)
+    rookGraphTest(5,10,9)
+if __name__=="__main__":
+    bora()
+    #s = Test()
+    #graphTest(5,10,0)
