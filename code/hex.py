59021a02817387b80a7f20f8c4706aa04c5f9bf3
Charles Schmidt <schmidtc@users.noreply.github.com>
2008-02-15 04:37:38 +0000

    hexGraph and hexPts seperates the creation of the graph from the plotting of the node locations

--- code/hex.py
+++ code/hex.py
@@ -1,61 +1,62 @@
-import pylab
 import networkx
 from math import sqrt
-x = 7
-y = 8
-
-deltaX = 1.0
-deltaY = sqrt(3.0/4.0)
-
-hexX=0-deltaX
-hexY=0
-pts = []
-grid = []
-c = 0
-for lcvY in range(y):
-    row = []
-    for lcvX in range(x):
-        hexX+=deltaX
-        pts.append([hexX,hexY])
-        row.append(c)
-        c += 1
-    if (lcvY+1)%2:
-        hexX=0-(deltaX/2)
-    else:
-        hexX=0-deltaX
-    hexY=hexY+deltaY
-    grid.append(row)
-
-x,y = zip(*pts)
 
 
-g = networkx.Graph()
 
-for i,row in enumerate(grid):
-    # L * * *
-    # R  * * *
-    # L * * *
-    for j,ptID in enumerate(row):
-        if i%2: # R, can never be first row.
-            g.add_edge(ptID,grid[i-1][j]) #my upper neighbor
-            if j < len(row) - 1: #not last column
-                g.add_edge(ptID,grid[i][j+1]) #my right neighbor
-                g.add_edge(ptID,grid[i-1][j+1]) #my upper right neighbor
-            if i < len(grid) -1: #not last row
-                g.add_edge(ptID,grid[i+1][j]) #my lower neighbor
+def hexGraph(x,y):
+    grid = []
+    c = 0
+    for i in range(y):
+        row = []
+        for j in range(x):
+            row.append(c)
+            c += 1
+        grid.append(row)
+    g = networkx.Graph()
+    for i,row in enumerate(grid):
+        # L * * *
+        # R  * * *
+        # L * * *
+        for j,ptID in enumerate(row):
+            if i%2: # R, can never be first row.
+                g.add_edge(ptID,grid[i-1][j]) #my upper neighbor
                 if j < len(row) - 1: #not last column
-                    g.add_edge(ptID,grid[i+1][j+1]) #my lower right neighbor
+                    g.add_edge(ptID,grid[i][j+1]) #my right neighbor
+                    g.add_edge(ptID,grid[i-1][j+1]) #my upper right neighbor
+                if i < len(grid) -1: #not last row
+                    g.add_edge(ptID,grid[i+1][j]) #my lower neighbor
+                    if j < len(row) - 1: #not last column
+                        g.add_edge(ptID,grid[i+1][j+1]) #my lower right neighbor
+            else: # L
+                if j < len(row)-1:
+                    g.add_edge(ptID,grid[i][j+1]) #my right neighbor
+    return g
 
-        else: # L
-            if j < len(row)-1:
-                g.add_edge(ptID,grid[i][j+1]) #my right neighbor
+def hexPts(x,y):
+    deltaX = 1.0
+    deltaY = sqrt(3.0/4.0)
+    hexX=0-deltaX
+    hexY=0
+    pts = []
+    for lcvY in range(y):
+        for lcvX in range(x):
+            hexX+=deltaX
+            pts.append([hexX,hexY])
+        if (lcvY+1)%2:
+            hexX=0-(deltaX/2)
+        else:
+            hexX=0-deltaX
+        hexY=hexY+deltaY
+    return pts
 
-        
 if __name__=='__main__':
     import Tkinter as tk
     root = tk.Tk()
     c = tk.Canvas(root, width=800,height=800)
     c.pack()
+    
+    g = hexGraph(5,5)
+    pts = hexPts(5,5)
 
     for a,b in g.edges():
         x1,y1 = pts[a]
