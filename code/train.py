f3a0055ae56017924a4943a19e02ed1c2b577cd2
Charles Schmidt <schmidtc@users.noreply.github.com>
2008-02-27 07:15:25 +0000

    getting read for odd and even hex

--- code/train.py
+++ code/train.py
@@ -8,23 +8,23 @@ def graphTrain(G,type,dims,clusters,testNum):
     s.maxN = 0.5
     s.tSteps = 100000
     s.alpha0 = 0.04
-    f = ObsFile('testData/%dd-%dc-no%d_rs.dat'%(dims,clusters,testNum),'complete')
+    f = ObsFile('../data/trainingData/%dd-%dc-no%d_rs.dat'%(dims,clusters,testNum),'complete')
     print "init"
     s.randInit()
     print "run t=100K"
     s.run(f)
-    s.save('testResults/','%s_%dd-%dc-no%d_100k'%(type,dims,clusters,testNum))
+    s.save('../data/trainedSOMs/','%s_%dd-%dc-no%d_100k'%(type,dims,clusters,testNum))
     s.maxN = 0.333
     s.tSteps = 1000000
     s.alpha0 = 0.03
     f.reset()
     print "run t=1M"
     s.run(f)
-    s.save('testResults/','%s_%dd-%dc-no%d_1m'%(type,dims,clusters,testNum))
+    s.save('../data/trainedSOMs/','%s_%dd-%dc-no%d_1m'%(type,dims,clusters,testNum))
 
     f.reset()
     s.map(f)
-    s.save('testResults/','%s_%dd-%dc-no%d_1m'%(type,dims,clusters,testNum))
+    s.save('../data/trainedSOMs/','%s_%dd-%dc-no%d_1m'%(type,dims,clusters,testNum))
 
     f.close()
     return s
@@ -34,51 +34,54 @@ if __name__=="__main__":
     from hex import hexGraph
 
     #build graphs
-    geodesic = delaunay.parseDelaunay("geodesic/geodesic_642_delaunay.xyz")
-    sphere = delaunay.parseDelaunay("delaunay/642_delaunay.xyz")
-    g = grid2Rook(23,28,binary=1)
-    rook = NX.Graph()
-    for node in g:
-        for neighbor in g[node][1]:
-            rook.add_edge((node,neighbor))
-    hex = hexGraph(23,28)
+    #geodesic = delaunay.parseDelaunay("geodesic/geodesic_642_delaunay.xyz")
+    #sphere = delaunay.parseDelaunay("delaunay/642_delaunay.xyz")
+    #g = grid2Rook(23,28,binary=1)
+    #rook = NX.Graph()
+    #for node in g:
+    #    for neighbor in g[node][1]:
+    #        rook.add_edge((node,neighbor))
+    #hex = hexGraph(23,28)
+    hexOdd = hexGraph(25,25)
+    hexEven = hexGraph(24,26)
 
     #graphTrain(sphere,'Sphere',5,10,0)
     #graphTrain(rook,'Rook',5,10,0)
-    #graphTrain(hex,'Hex',5,10,0)
+    graphTrain(hexOdd,'HexOdd',5,10,0)
+    graphTrain(hexOdd,'HexOdd',5,10,1)
+    graphTrain(hexOdd,'HexOdd',5,10,2)
+    graphTrain(hexOdd,'HexOdd',5,10,3)
+    graphTrain(hexOdd,'HexOdd',5,10,4)
+    #graphTrain(hexOdd,'HexOdd',5,10,5)
+    #graphTrain(hexOdd,'HexOdd',5,10,6)
+    #graphTrain(hexOdd,'HexOdd',5,10,7)
+    #graphTrain(hexOdd,'HexOdd',5,10,8)
+    #graphTrain(hexOdd,'HexOdd',5,10,9)
+    #graphTrain(hexEven,'HexEven',5,10,0)
+    #graphTrain(hexEven,'HexEven',5,10,1)
+    #graphTrain(hexEven,'HexEven',5,10,2)
+    #graphTrain(hexEven,'HexEven',5,10,3)
+    #graphTrain(hexEven,'HexEven',5,10,4)
+    #graphTrain(hexEven,'HexEven',5,10,5)
+    #graphTrain(hexEven,'HexEven',5,10,6)
+    #graphTrain(hexEven,'HexEven',5,10,7)
+    #graphTrain(hexEven,'HexEven',5,10,8)
+    #graphTrain(hexEven,'HexEven',5,10,9)
 
-    def bora():
-        #graphTrain(geodesic,'geodesic',5,10,0)
-        #graphTrain(geodesic,'geodesic',5,10,1)
-        #graphTrain(geodesic,'geodesic',5,10,2)
-    def neve():
-        #graphTrain(geodesic,'geodesic',5,10,3)
-        #graphTrain(geodesic,'geodesic',5,10,4)
-    def sheldon():
-        hex = hexGraph(23,28)
-        graphTrain(hex,'size_test_23_28',5,10,0)
-        hex = hexGraph(23,23)
-        graphTrain(hex,'size_test_23_23',5,10,0)
-        hex = hexGraph(20,20)
-        graphTrain(hex,'size_test_20_20',5,10,0)
-        hex = hexGraph(15,15)
-        graphTrain(hex,'size_test_15_15',5,10,0)
-        hex = hexGraph(10,10)
-        graphTrain(hex,'size_test_10_10',5,10,0)
-        hex = hexGraph(5,5)
-        graphTrain(hex,'size_test_5_5',5,10,0)
-        #graphTrain(geodesic,'geodesic',5,10,6)
-        #graphTrain(geodesic,'geodesic',5,10,7)
-    def haar():
-        #graphTrain(geodesic,'geodesic',5,10,8)
-        #graphTrain(geodesic,'geodesic',5,10,9)
+    #def bora():
+    #    pass
+    #def neve():
+    #    pass
+    #def sheldon():
+    #    pass
+    #def haar():
+    #    pass
 
-
-    import socket
-    hostname = socket.gethostname()
-    host = hostname.split('.')[0]
-    dispatch = {'bora':bora,'neve':neve,'haar':haar,'sheldon':sheldon}
-    dimSet = dispatch[host]()
+    #import socket
+    #hostname = socket.gethostname()
+    #host = hostname.split('.')[0]
+    #dispatch = {'bora':bora,'neve':neve,'haar':haar,'sheldon':sheldon}
+    #dispatch[host]()
 
 
 
