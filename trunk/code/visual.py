e0cd099d6256c859520703d0d02b83967dd98090
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2008-03-09 20:06:14 +0000

    visual not outputs a DBF with only IV values, if a '*' is in the place of no*

--- trunk/code/visual.py
+++ trunk/code/visual.py
@@ -1,19 +1,15 @@
 from utils import dbf
 import sys
 
-if len(sys.argv) < 2:
-    print "Usage: python visual.py /path/to/som.cod /path/to/som.ib"
-else:
-    fname = sys.argv[1]
-
+def read(fname,ivfileName,pre=''):
     f = open(fname,'r')
     head = f.readline()
     data = f.readlines()
     f.close()
 
     dims = int(head.split()[0])
-    names = ['ID']
-    names.extend(['DIM'+str(i+1) for i in range(dims)])
+    names = [pre+'ID']
+    names.extend([pre+'DIM'+str(i+1) for i in range(dims)])
     specs = [('N', 10, 0)]
     specs.extend([('N', 19, 11) for i in range(dims)])
     
@@ -24,12 +20,12 @@ else:
         rec.extend(d)
         records.append(rec)
     
-    if len(sys.argv) > 2:
-        ivfile = open(sys.argv[2],'r')
-        names.extend(['Size','Degree','IV'])
-        specs.extend([('N', 10, 0),('N', 10, 0),('N',19,11)])
-        for rec in records:
-            rec.extend([0,-99,-99.0])
+    if 1:
+        ivfile = open(ivfileName,'r')
+        names.extend([pre+'Size',pre+'Degree',pre+'IV'])
+        specs.extend([('N', 11, 0),('N', 10, 0),('N',19,11)])
+        for i in xrange(len(records)):
+            records[i].extend([0,-99,-99.0])
         for line in ivfile:
             nid,size,deg,iv = line.split(',')
             nid = int(nid)
@@ -41,9 +37,44 @@ else:
             records[nid][-1] = iv
         ivfile.close()
     
+    return names,specs,records
+
+def write(names,specs,records):
     f = open('visual.dbf','wb')
     #names = ['ID','AFIELD','BFIELD','CFIELD','DFIELD','EFIELD','GFIELD']
     #specs = [('N', 10, 0), ('N', 19, 11), ('N', 19, 11), ('N', 19,11), ('N', 19,11), ('N', 19,11), ('N', 19, 11)]
     dbf.dbfwriter(f,names,specs,records)
     f.close()
 
+
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print "Usage: python visual.py /path/to/som.cod /path/to/som.iv"
+    else:
+        fname = sys.argv[1]
+        ivfile = sys.argv[2]
+
+    if '*' not in fname:
+        names,specs,recs = read(fname,ivfile)
+    else:
+        fn = fname.replace('*',str(0))
+        iv = ivfile.replace('*',str(0))
+        names,specs,recs = read(fn,iv,pre='no%d_'%0)
+        outNames = names[:1]
+        outNames.append(names[-1])
+        outSpecs = specs[:1]
+        outSpecs.append(specs[-1])
+        outRecs = []
+        for r in recs:
+            outRecs.append([r[0],r[-1]])
+        
+        for i in range(1,10):
+            fn = fname.replace('*',str(i))
+            iv = ivfile.replace('*',str(i))
+            names,specs,recs = read(fn,iv,pre='no%d_'%i)
+            outNames.append(names[-1])
+            outSpecs.append(specs[-1])
+            for i,r in enumerate(recs):
+                outRecs[i].append(r[-1])
+        write(outNames,outSpecs,outRecs)
