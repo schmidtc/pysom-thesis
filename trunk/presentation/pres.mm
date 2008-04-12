857f24d8405dd4dfbeec9a982f6dad6eeff6552d
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2008-04-12 19:50:24 +0000

    a start.

---
+++ trunk/presentation/pres.mm
@@ -0,0 +1,184 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1146374668037" ID="Freemind_Link_1802285529" MODIFIED="1146374668037" TEXT="New Mindmap">
+<node CREATED="1146374681942" ID="Freemind_Link_110956540" MODIFIED="1146374681942" POSITION="right" TEXT="preamble">
+<node CREATED="1146374681943" ID="Freemind_Link_1672812378" MODIFIED="1207801605858" TEXT="@author Charles R. Schmidt\\Sergio J. Rey\\Andr\&apos;e Skupin"/>
+<node CREATED="1207802297385" ID="Freemind_Link_382373296" MODIFIED="1207802336433" TEXT="@authorShort Schmidt, Rey, Skupin"/>
+<node CREATED="1146374681943" ID="Freemind_Link_1546084328" MODIFIED="1207802286155" TEXT="@institute Department of Geography\\San Diego State University"/>
+<node CREATED="1146374681948" ID="Freemind_Link_1212274194" MODIFIED="1207802291434" TEXT="@instituteShort SDSU"/>
+<node CREATED="1146374681948" ID="Freemind_Link_1946700144" MODIFIED="1207801634012" TEXT="@title Effects of Irregular Topology in Spherical Self-Organizing Maps"/>
+<node CREATED="1146374681949" ID="Freemind_Link_960470390" MODIFIED="1207801672480" TEXT="@titleShort Irregular Topology in Spherical-SOM"/>
+<node CREATED="1146374681949" ID="Freemind_Link_1997546210" MODIFIED="1207801767473" TEXT="@date April 18, 2008"/>
+<node CREATED="1146374681964" ID="Freemind_Link_1809338804" MODIFIED="1207801773695" TEXT="@dateShort AAG"/>
+<node CREATED="1146425704339" ID="Freemind_Link_1106806043" MODIFIED="1146425704339" TEXT="\definecolor{sdsu_red}{RGB}{155,200,115}"/>
+<node CREATED="1146425714345" ID="Freemind_Link_1777296336" MODIFIED="1207801792493" TEXT="\usecolortheme[named=sdsu_red]{structure}"/>
+</node>
+<node CREATED="1146374681964" FOLDED="true" ID="Freemind_Link_66840693" MODIFIED="1146374973260" POSITION="left" TEXT="Section">
+<node CREATED="1146374681964" ID="Freemind_Link_1560141715" MODIFIED="1146374981748" TEXT="Sub-Section">
+<node CREATED="1146374681965" ID="Freemind_Link_667075695" MODIFIED="1146375093670" TEXT="Frame">
+<node CREATED="1146374682013" ID="Freemind_Link_1478146944" MODIFIED="1146375106726" TEXT="block"/>
+<node CREATED="1146374682014" MODIFIED="1146374682014" TEXT="|* Data oriented: emphasize representation of information , or"/>
+<node CREATED="1146374682014" MODIFIED="1146374682014" TEXT="|* Process oriented: emphasize the actions performed"/>
+<node CREATED="1146374682015" MODIFIED="1146374682015" TEXT="|* Programs emphasize one or the other but not both"/>
+<node CREATED="1146374682017" ID="Freemind_Link_1470897897" MODIFIED="1146375637944" TEXT="block"/>
+<node CREATED="1146374682018" MODIFIED="1146374682018" TEXT="| * \emph{Object-oriented} methodology"/>
+<node CREATED="1146374682018" MODIFIED="1146374682018" TEXT="| * Data and processes given equal importance"/>
+<node CREATED="1146374682019" MODIFIED="1146374682019" TEXT="| * \alert{Objects} combine:"/>
+<node CREATED="1146374682020" MODIFIED="1146374682020" TEXT="| ** Data"/>
+<node CREATED="1146374682020" ID="Freemind_Link_1219816539" MODIFIED="1146374682020" TEXT="| *** Procedures operating on the data"/>
+</node>
+<node CREATED="1146374682020" ID="Freemind_Link_949639243" MODIFIED="1146375652360" TEXT="Frame2">
+<node CREATED="1146374682021" ID="Freemind_Link_1886852366" MODIFIED="1146375644648" TEXT="block"/>
+<node CREATED="1146374682021" MODIFIED="1146374682021" TEXT="| OOP scales very nicely with the complexity found in most large"/>
+<node CREATED="1146374682021" MODIFIED="1146374682021" TEXT="| programming projects."/>
+<node CREATED="1146374682022" ID="Freemind_Link_840414439" MODIFIED="1146375647768" TEXT="block"/>
+<node CREATED="1146374682022" MODIFIED="1146374682022" TEXT="| OOP gives us an approach where we can think about the problem"/>
+<node CREATED="1146374682023" MODIFIED="1146374682023" TEXT="| domain more directly."/>
+</node>
+</node>
+</node>
+<node CREATED="1207620283541" FOLDED="true" ID="Freemind_Link_1831689683" MODIFIED="1207620292821" POSITION="right" TEXT="Introduction">
+<node CREATED="1207620293402" ID="Freemind_Link_400996438" MODIFIED="1207620307756" TEXT="The Self-Organizing Map">
+<node CREATED="1207620327788" ID="Freemind_Link_925583082" MODIFIED="1207801317500" TEXT="What is SOM">
+<node CREATED="1207620341129" ID="Freemind_Link_1947719134" MODIFIED="1207801332457" TEXT="| SOM is:"/>
+<node CREATED="1207801333350" ID="Freemind_Link_492784755" MODIFIED="1207801346230" TEXT="| "/>
+</node>
+</node>
+<node CREATED="1207620308225" ID="Freemind_Link_1130544780" MODIFIED="1207620312278" TEXT="The Edge-Effect"/>
+<node CREATED="1207620312643" ID="Freemind_Link_1507554623" MODIFIED="1207620322235" TEXT="Spherical SOM"/>
+<node CREATED="1146440901528" ID="Freemind_Link_263537332" MODIFIED="1146441602105" TEXT="The Goals">
+<node CREATED="1146441010259" ID="Freemind_Link_239647202" MODIFIED="1146441604409" TEXT="The Goals">
+<node CREATED="1146440928977" ID="Freemind_Link_41959249" MODIFIED="1146441521367" TEXT="| Create a System to Regionalization the National IO Table that is:"/>
+<node CREATED="1146440971714" ID="Freemind_Link_1904580965" MODIFIED="1146441316739" TEXT="| * Expandible"/>
+<node CREATED="1146441066517" ID="Freemind_Link_1571400106" MODIFIED="1146441079295" TEXT="| * Usable"/>
+<node CREATED="1146441080051" ID="Freemind_Link_1191071799" MODIFIED="1146441085631" TEXT="| * Open Source"/>
+<node CREATED="1146441086028" ID="Freemind_Link_472565408" MODIFIED="1146441184825" TEXT="| * \alert{Free \&amp; Cheap}"/>
+<node CREATED="1146441187301" ID="Freemind_Link_265774465" MODIFIED="1146441209321" TEXT="| ** \$: No Cost"/>
+<node CREATED="1146441210542" ID="Freemind_Link_1053089849" MODIFIED="1146441230610" TEXT="| ** Time: Minimize"/>
+</node>
+</node>
+</node>
+<node CREATED="1207622400293" FOLDED="true" ID="Freemind_Link_1239392290" MODIFIED="1207622404738" POSITION="right" TEXT="Background">
+<node CREATED="1207622407072" ID="Freemind_Link_1068937070" MODIFIED="1207622426020" TEXT="SOM Overview">
+<node CREATED="1207622426593" ID="Freemind_Link_1545106596" MODIFIED="1207622428981" TEXT="Training"/>
+<node CREATED="1207622429370" ID="Freemind_Link_721521058" MODIFIED="1207622436921" TEXT="Edge Effect"/>
+</node>
+<node CREATED="1207622441647" ID="Freemind_Link_1664870472" MODIFIED="1207622446693" TEXT="Spherical SOM"/>
+<node CREATED="1207622447586" ID="Freemind_Link_1152660447" MODIFIED="1207622450000" TEXT="Network Size"/>
+<node CREATED="1207622450948" ID="Freemind_Link_263314150" MODIFIED="1207622454449" TEXT="Uniformity"/>
+</node>
+<node CREATED="1207622458087" FOLDED="true" ID="Freemind_Link_524613033" MODIFIED="1207622465951" POSITION="right" TEXT="Methodology">
+<node CREATED="1207622468252" ID="Freemind_Link_1355141236" MODIFIED="1207622473082" TEXT="Diagnostics">
+<node CREATED="1207622474559" ID="Freemind_Link_1651316955" MODIFIED="1207622507994" TEXT="IV vs. 1st order"/>
+<node CREATED="1207622525318" ID="Freemind_Link_1968878996" MODIFIED="1207622557233" TEXT="IV vs Regularity"/>
+<node CREATED="1207622562472" ID="Freemind_Link_898878178" MODIFIED="1207622566693" TEXT="Visualiztions"/>
+</node>
+</node>
+<node CREATED="1207622569635" ID="Freemind_Link_1814552153" MODIFIED="1207622571888" POSITION="right" TEXT="Results">
+<node CREATED="1207622575582" ID="Freemind_Link_1549491376" MODIFIED="1207622581658" TEXT="IV 1">
+<node CREATED="1207622582225" ID="Freemind_Link_1933433853" MODIFIED="1207622583525" TEXT="IV 1"/>
+</node>
+<node CREATED="1207622585258" ID="Freemind_Link_1177331514" MODIFIED="1207622587455" TEXT="IV 2">
+<node CREATED="1207622588244" ID="Freemind_Link_73009021" MODIFIED="1207622589712" TEXT="IV 2"/>
+</node>
+<node CREATED="1207622590653" ID="Freemind_Link_34165475" MODIFIED="1207622592529" TEXT="Visual">
+<node CREATED="1207622593046" ID="Freemind_Link_1963276080" MODIFIED="1207622594578" TEXT="Visual"/>
+</node>
+</node>
+<node CREATED="1146374681964" FOLDED="true" ID="Freemind_Link_1438216664" MODIFIED="1207620282634" POSITION="left" TEXT="Example Intro">
+<node CREATED="1146374681964" ID="Freemind_Link_300128288" MODIFIED="1146430162165" TEXT="Input-Output">
+<node CREATED="1146374681965" ID="Freemind_Link_913201053" MODIFIED="1146430172821" TEXT="What is Input-Output">
+<node CREATED="1146374682014" ID="Freemind_Link_490506192" MODIFIED="1146376312444" TEXT="Industy by Industy Maxtrix"/>
+<node CREATED="1146383529262" ID="Freemind_Link_527141353" MODIFIED="1146383543417" TEXT="| \begin{center}"/>
+<node CREATED="1146377462525" ID="Freemind_Link_450901267" MODIFIED="1146429773310" TEXT="| \begin{tabular}{l|ccc|ccc}"/>
+<node CREATED="1146377497645" ID="Freemind_Link_1860617077" MODIFIED="1146383302244" TEXT="| &amp;\multicolumn{3}{c}{\textbf{Buyers}}&amp;\multicolumn{2}{c}{\textbf{Final Demand}}\\"/>
+<node CREATED="1146376879491" ID="Freemind_Link_9415252" MODIFIED="1146428861805" TEXT="| \textbf{Suppliers} &amp;Ind 1&amp;Ind 2&amp;Ind 3&amp;Exports&amp;Govt&amp;HH\\ \hline"/>
+<node CREATED="1146377680721" ID="Freemind_Link_587151532" MODIFIED="1146428871677" TEXT="| Ind 1&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382041786" ID="Freemind_Link_272675691" MODIFIED="1146428879358" TEXT="| Ind 2&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382045842" ID="Freemind_Link_1624722813" MODIFIED="1146428885630" TEXT="| Ind 3&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146428190798" ID="Freemind_Link_1517687000" MODIFIED="1146428195289" TEXT="| \hline"/>
+<node CREATED="1146428829129" ID="Freemind_Link_1814866150" MODIFIED="1146428846317" TEXT="| Imports&amp;x&amp;x&amp;x&amp;&amp;\\"/>
+<node CREATED="1146428429298" ID="Freemind_Link_720943419" MODIFIED="1146428489438" TEXT="| Tax&amp;x&amp;x&amp;x&amp;&amp;\\"/>
+<node CREATED="1146428196310" ID="Freemind_Link_1548404475" MODIFIED="1146428242026" TEXT="| Labor&amp;x&amp;x&amp;x&amp;&amp;"/>
+<node CREATED="1146377918285" ID="_" MODIFIED="1146377926465" TEXT="| \end{tabular}"/>
+<node CREATED="1146383545397" ID="Freemind_Link_1576534188" MODIFIED="1146383555817" TEXT="| \end{center}"/>
+</node>
+</node>
+<node CREATED="1146430205434" ID="Freemind_Link_639173717" MODIFIED="1146430213558" TEXT="Regional Input-Output">
+<node CREATED="1146374682020" FOLDED="true" ID="Freemind_Link_1592763481" MODIFIED="1146430184237" TEXT="What is Regional IO">
+<node CREATED="1146374682014" ID="Freemind_Link_1512004918" MODIFIED="1146427137734" TEXT="Single Region - Regional IO"/>
+<node CREATED="1146377462525" ID="Freemind_Link_462969365" MODIFIED="1146429785422" TEXT="| \begin{tabular}{l|ccc}"/>
+<node CREATED="1146377497645" ID="Freemind_Link_912906559" MODIFIED="1146428776676" TEXT="| Reg A&amp;\multicolumn{3}{c}{\textbf{Buyers}}\\"/>
+<node CREATED="1146376879491" ID="Freemind_Link_1168818363" MODIFIED="1146428525383" TEXT="| \textbf{Suppliers} &amp;Ind 1&amp;Ind 2&amp;Ind 3\\ \hline"/>
+<node CREATED="1146377680721" ID="Freemind_Link_1804650809" MODIFIED="1146428528863" TEXT="| Ind 1&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382041786" ID="Freemind_Link_533800595" MODIFIED="1146428532096" TEXT="| Ind 2&amp;x&amp;x&amp;x\\ "/>
+<node CREATED="1146382045842" ID="Freemind_Link_1500083245" MODIFIED="1146428534815" TEXT="| Ind 3&amp;x&amp;x&amp;x"/>
+<node CREATED="1146377918285" ID="Freemind_Link_437783460" MODIFIED="1146429159824" TEXT="| \end{tabular}"/>
+<node CREATED="1146429171128" ID="Freemind_Link_944735405" MODIFIED="1146429577667" TEXT="| \begin{flushright}"/>
+<node CREATED="1146377462525" ID="Freemind_Link_452537381" MODIFIED="1146429791406" TEXT="| \begin{tabular}{l|ccc}"/>
+<node CREATED="1146377497645" ID="Freemind_Link_658481113" MODIFIED="1146428762149" TEXT="| Reg B&amp;\multicolumn{3}{c}{\textbf{Buyers}}\\"/>
+<node CREATED="1146376879491" ID="Freemind_Link_920942328" MODIFIED="1146428525383" TEXT="| \textbf{Suppliers} &amp;Ind 1&amp;Ind 2&amp;Ind 3\\ \hline"/>
+<node CREATED="1146377680721" ID="Freemind_Link_1090449848" MODIFIED="1146428528863" TEXT="| Ind 1&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382041786" ID="Freemind_Link_388859475" MODIFIED="1146428532096" TEXT="| Ind 2&amp;x&amp;x&amp;x\\ "/>
+<node CREATED="1146382045842" ID="Freemind_Link_1257492658" MODIFIED="1146428534815" TEXT="| Ind 3&amp;x&amp;x&amp;x"/>
+<node CREATED="1146377918285" ID="Freemind_Link_1881201652" MODIFIED="1146377926465" TEXT="| \end{tabular}"/>
+<node CREATED="1146383545397" ID="Freemind_Link_239428335" MODIFIED="1146429583026" TEXT="| \end{flushright}"/>
+</node>
+<node CREATED="1146428654014" FOLDED="true" ID="Freemind_Link_230329629" MODIFIED="1146430202934" TEXT="What is Regional IO">
+<node CREATED="1146427165203" ID="Freemind_Link_1381225589" MODIFIED="1146427187383" TEXT="Multi-region - Regional IO"/>
+<node CREATED="1146383529262" ID="Freemind_Link_1115002562" MODIFIED="1146383543417" TEXT="| \begin{center}"/>
+<node CREATED="1146377462525" ID="Freemind_Link_1596818897" MODIFIED="1146429802255" TEXT="| \begin{tabular}{ll|ccc|ccc}"/>
+<node CREATED="1146429878020" ID="Freemind_Link_923880249" MODIFIED="1146429906000" TEXT="| &amp;&amp;\multicolumn{6}{c}{\textbf{Buyers}}\\"/>
+<node CREATED="1146377497645" ID="Freemind_Link_292532845" MODIFIED="1146430013490" TEXT="| &amp;&amp;\multicolumn{3}{c}{\textbf{Reg 1}}&amp;\multicolumn{3}{c}{\textbf{Reg 2}}\\"/>
+<node CREATED="1146376879491" ID="Freemind_Link_198651046" MODIFIED="1146443302482" TEXT="| \multicolumn{2}{c|}{\textbf{Suppliers}}&amp;Ind 1&amp;Ind 2&amp;Ind 3&amp;Ind 1&amp;Ind 2&amp;Ind 3\\ \hline"/>
+<node CREATED="1146427504273" ID="Freemind_Link_1429615287" MODIFIED="1146428064903" TEXT="| &amp;Ind 1&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146377680721" ID="Freemind_Link_1732271236" MODIFIED="1146428070071" TEXT="| \textbf{Reg 1}&amp;Ind 2&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382041786" ID="Freemind_Link_1731823350" MODIFIED="1146428073799" TEXT="| &amp;Ind 3&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146427910585" ID="Freemind_Link_480119110" MODIFIED="1146427914756" TEXT="| \hline"/>
+<node CREATED="1146427504273" ID="Freemind_Link_176162572" MODIFIED="1146428078055" TEXT="| &amp;Ind 1&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146377680721" ID="Freemind_Link_1375473143" MODIFIED="1146428081112" TEXT="| \textbf{Reg 2}&amp;Ind 2&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146382041786" ID="Freemind_Link_928716231" MODIFIED="1146428092055" TEXT="| &amp;Ind 3&amp;x&amp;x&amp;x&amp;x&amp;x&amp;x\\"/>
+<node CREATED="1146377918285" ID="Freemind_Link_1121737545" MODIFIED="1146377926465" TEXT="| \end{tabular}"/>
+<node CREATED="1146383545397" ID="Freemind_Link_1773010654" MODIFIED="1146383555817" TEXT="| \end{center}"/>
+</node>
+</node>
+<node CREATED="1146426920056" FOLDED="true" ID="Freemind_Link_911122246" MODIFIED="1146430257455" TEXT="What&apos;s Being Done Now">
+<node CREATED="1146423852655" ID="Freemind_Link_1947832122" MODIFIED="1146430600418" TEXT="Ready Made Tables">
+<node CREATED="1146430629732" ID="Freemind_Link_84406272" MODIFIED="1146430634973" TEXT="IMPLAN"/>
+<node CREATED="1146430825237" ID="Freemind_Link_1121109155" MODIFIED="1146430838945" TEXT="| * Single Regional Tables"/>
+<node CREATED="1146430839342" ID="Freemind_Link_833780366" MODIFIED="1146430868683" TEXT="| ** Every County in the United States"/>
+<node CREATED="1146430346373" ID="Freemind_Link_1834102443" MODIFIED="1146430976716" TEXT="| *** \alert{\$35,000}"/>
+<node CREATED="1146431160755" ID="Freemind_Link_1753739639" MODIFIED="1146431198160" TEXT="| ** \alert{\$175 per county}"/>
+<node CREATED="1146430739940" ID="Freemind_Link_1591227710" MODIFIED="1146430741728" TEXT="REMI"/>
+<node CREATED="1146430890039" ID="Freemind_Link_1611049836" MODIFIED="1146430945219" TEXT="| * Multi-Regional Tables"/>
+<node CREATED="1146430946903" ID="Freemind_Link_1495004618" MODIFIED="1146440783162" TEXT="| ** \alert{Very expensive}"/>
+<node CREATED="1146430980296" ID="Freemind_Link_1776920658" MODIFIED="1146430984020" TEXT="RIMS II"/>
+<node CREATED="1146430984729" ID="Freemind_Link_1252325831" MODIFIED="1146431000372" TEXT="| * Multiplers Only"/>
+<node CREATED="1146431001032" ID="Freemind_Link_1042141197" MODIFIED="1146431017165" TEXT="| ** Short Cut Methods, no Tables produced"/>
+<node CREATED="1146431030593" ID="Freemind_Link_1616723705" MODIFIED="1146431222568" TEXT="| *** \alert{\$275 per region}"/>
+</node>
+<node CREATED="1146431254973" ID="Freemind_Link_1101743853" MODIFIED="1146431260105" TEXT="Roll Your Own">
+<node CREATED="1146431260653" ID="Freemind_Link_1921395222" MODIFIED="1146431265297" TEXT="Methods"/>
+<node CREATED="1146431265965" ID="Freemind_Link_1010042176" MODIFIED="1146431275225" TEXT="| * Many Options"/>
+<node CREATED="1146431276045" ID="Freemind_Link_1914844205" MODIFIED="1146431316090" TEXT="| ** Widely Documented"/>
+<node CREATED="1146431316511" ID="Freemind_Link_761699137" MODIFIED="1146431325274" TEXT="| *** Textbooks"/>
+<node CREATED="1146431325678" ID="Freemind_Link_1318421234" MODIFIED="1146431332738" TEXT="| *** Articles"/>
+<node CREATED="1146431357031" ID="Freemind_Link_1751481711" MODIFIED="1146431364731" TEXT="Data"/>
+<node CREATED="1146431365135" ID="Freemind_Link_1045046292" MODIFIED="1146431376395" TEXT="| * Widely Avalible"/>
+<node CREATED="1146431376959" ID="Freemind_Link_797976879" MODIFIED="1146431388435" TEXT="| ** Nation IO Table"/>
+<node CREATED="1146431388816" ID="Freemind_Link_1343638979" MODIFIED="1146431394867" TEXT="| *** Free Download"/>
+<node CREATED="1146431395368" ID="Freemind_Link_868721701" MODIFIED="1146431413566" TEXT="| ** Census County Business Patterns"/>
+<node CREATED="1146431414200" ID="Freemind_Link_1112665450" MODIFIED="1146431998806" TEXT="| *** Available on CD for \$50"/>
+</node>
+<node CREATED="1146431961562" ID="Freemind_Link_1653788934" MODIFIED="1146431969542" TEXT="Roll Your Own - Cont.">
+<node CREATED="1146431530418" ID="Freemind_Link_1188777187" MODIFIED="1146434590581" TEXT="Why Not?"/>
+<node CREATED="1146431576483" ID="Freemind_Link_1533262983" MODIFIED="1146431585639" TEXT="| * Investment"/>
+<node CREATED="1146431540130" ID="Freemind_Link_1131650904" MODIFIED="1146431610863" TEXT="| ** Time"/>
+<node CREATED="1146431549034" ID="Freemind_Link_1478993191" MODIFIED="1146431633903" TEXT="| ** Knowledge"/>
+<node CREATED="1146431635252" ID="Freemind_Link_1856448114" MODIFIED="1146431639248" TEXT="| *** Methods"/>
+<node CREATED="1146431640052" ID="Freemind_Link_1515078545" MODIFIED="1146431644696" TEXT="| *** Computation"/>
+</node>
+</node>
+</node>
+</node>
+</map>
