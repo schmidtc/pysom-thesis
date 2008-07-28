7caa9afecc20d0e20a37b6852d15edfb9eb7c987
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2008-07-28 19:00:47 +0000

    cleaning up code for appendix

--- trunk/code/dataGen.py
+++ trunk/code/dataGen.py
@@ -4,69 +4,6 @@ import pylab
 import random
 from scaler import scaler
 
-
-#def gaussian(nDims=10,nClusters=10,minClusterSize=100,maxClusterSize=1000):
-#    size = [random.randint(minClusterSize,maxClusterSize) for i in xrange(nClusters)]
-#    #Generate mean values
-#    mean = [[random.random() for j in xrange(nDims)] for i in xrange(nClusters)]
-#    for i in xrange(nClusters):
-#        for j in xrange(nDims):
-#            #mean[j] = -MAXMU + random.random() * (MAXMU-MINMU);
-#            mean[j] = random.random()
-        
-    
-def gen(n,clusters,dims,noise):
-    """ 
-    eg.
-    n = 10000 #Sample size
-    clusters = 5 #The number of clusters
-    dims = 20 #the number of dimmensions
-    noise = 0.05 #5% random noise
-
-    returns a dictionary of lists
-    """
-
-    #generate seeds...
-    #seedr = [0.1 for i in xrange(clusters)]
-    def reject():
-        seeds = N.array([[random.random() for i in xrange(dims)] for j in xrange(clusters)])
-        seedr = [random.uniform(0,0.2) for i in xrange(clusters)]
-        for i in xrange(clusters):
-            for j in xrange(i+1,clusters):
-                d = N.sqrt(sum((seeds[i]-seeds[j])**2))
-                print d
-                if d < (seedr[i] + seedr[j]):
-                    return False
-        return seeds,seedr
-    while 1:
-        r = reject()
-        if r:
-            seeds,seedr = r
-            break
-
-
-    data = []
-    c = 0
-    while c < n:
-        pt = N.array([random.random() for i in xrange(dims)])
-        d = N.sqrt(((seeds-pt)**2).sum(1))
-        center = d.argmin()
-        if d.min() < seedr[center]:
-            pt = list(pt)
-            pt.append(center)
-            data.append(pt)
-            c += 1
-        else:
-            if random.random() < noise:
-                pt = list(pt)
-                pt.append(-1)
-                data.append(pt)
-                c += 1
-        sys.stdout.write("\r%d"%c)
-        sys.stdout.flush()
-        
-    return seeds,data
-
 def geo2006(fname='3d-7c-no%d', testNum=0, rand=True):
     """This function generates the same training data that Wu and Tkatsuka used in Geodesic 2006 paper, there are 3 dims, 7 clusters.
 
@@ -93,8 +30,6 @@ def geo2006(fname='3d-7c-no%d', testNum=0, rand=True):
             dists = N.sqrt(((cores-xyz)**2).sum(1))
             if min(dists) > 1.0:
                 rCluster.append(xyz)
-            #y = N.random.uniform(-10,10,3125)
-            #z = N.random.uniform(-10,10,3125)
 
         clusters.append(rCluster)
     
@@ -118,12 +53,3 @@ def geo2006(fname='3d-7c-no%d', testNum=0, rand=True):
 if __name__=="__main__":
     for i in range(10):
         geo2006(testNum=i, rand=False)
-    #s = gen(10000,7,2,0.00)
-    #x = [pt[0] for pt in s[1]]
-    #y = [pt[1] for pt in s[1]]
-
-    #pylab.hist(x,10000)
-    #pylab.scatter(x,y)
-    #pylab.xticks(pylab.arange(0,1.01,0.2))
-    #pylab.yticks(pylab.arange(0,1.01,0.2))
-    #pylab.show()
