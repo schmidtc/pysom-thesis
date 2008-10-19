9e2a8a958528c0f2deac1ab2ae7df7dd7dd10bff
Charles Schmidt <schmidtc@users.noreply.github.com>
2008-10-19 23:26:58 +0000

    re organizing code base

--- code/data.py
+++ code/data.py
@@ -1,25 +1,56 @@
 """
+======================================================================
 Python Self-Organizing Maps -- Data Tools
 ----------------------------------------------------------------------
 AUTHOR(S):      Charles R. Schmidt cschmidt@rohan.sdsu.edu
 ----------------------------------------------------------------------
-Copyright (c) 2006-2008  Charles R. Schmidt
-======================================================================
-This source code is probably licensed under the GNU General Public License,
-Version 2, you should check.
+* Copyright (c) 2006-2008, Charles R. Schmidt
+* All rights reserved.
+*
+* Redistribution and use in source and binary forms, with or without
+* modification, are permitted provided that the following conditions are met:
+*     * Redistributions of source code must retain the above copyright
+*       notice, this list of conditions and the following disclaimer.
+*     * Redistributions in binary form must reproduce the above copyright
+*       notice, this list of conditions and the following disclaimer in the
+*       documentation and/or other materials provided with the distribution.
+*     * Neither the name of the San Diego State University nor the
+*       names of its contributors may be used to endorse or promote products
+*       derived from this software without specific prior written permission.
+*
+* THIS SOFTWARE IS PROVIDED BY Charles R. Schmidt ''AS IS'' AND ANY
+* EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+* WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL Charles R. Schmidt BE LIABLE FOR ANY
+* DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+* (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+* LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+* ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+* (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+* SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ======================================================================
 """
+
 from numpy import array,empty
 
 class ObsFile:
+    ''' ObsFile provides an interface to your Observation Data File.
+        The File should be formated as follows:
+        The first line should contain only the number of dimmension in the data set.
+        Each subsequant line should contain one observation, each value should be seperated
+        a space.
+    '''
     def __init__(self,filename,fileType = 'complete'):
+        ''' Opens the file '''
         self.filename = filename
         self.fileObj = open(filename,'r')
         self.fileType = fileType
         self.reset()
     def __iter__(self):
+        ''' Create an interator '''
         return self
     def listolists(self,comments=False):
+        ''' Returns the entire data file as a list of lists '''
         self.fileObj.seek(0)
         lines = self.fileObj.readlines()
         dims = lines.pop(0)
@@ -32,6 +63,9 @@ class ObsFile:
             lines = [line[dims:] for line in lines]
         return lines
     def Snext(self):
+        ''' Returns the next line of a "sparse" data file.
+            A sparse data file is one that contains missing values
+        '''
         line = self.fileObj.next()
         id,line = line.split(':')
         line = line.split(',')
@@ -45,6 +79,9 @@ class ObsFile:
             c += 1
         return id,indices,values
     def Cnext(self):
+        ''' Return the next line of a "complete" data file.
+            A complete data file is one without any missing values.
+        '''
         line = self.fileObj.next()
         line = line.split()
         id = self.nextLine
@@ -53,6 +90,7 @@ class ObsFile:
         self.nextLine+=1
         return id,self.indices,self.values
     def reset(self):
+        ''' Resets the file such that the next line returned is the first line '''
         self.fileObj.seek(0)
         self.nextLine = 0 #Zero Base
         if self.fileType == 'complete':
@@ -65,10 +103,14 @@ class ObsFile:
         else:
             raise "fileTypeError"
     def stream(self):
+        ''' Loops of the records, always returns a line, if the end of file is reached, 
+            the file is seeked back to that start.
+        '''
         try:
             return self.next()
         except:
             self.reset()
             return self.next()
     def close(self):
+        ''' Close the file '''
         self.fileObj.close()
--- code/dataGen.py
+++ code/dataGen.py
@@ -7,7 +7,7 @@ from scaler import scaler
 def geo2006(fname='3d-7c-no%d', testNum=0, rand=True):
     """This function generates the same training data that Wu and Tkatsuka used in Geodesic 2006 paper, there are 3 dims, 7 clusters.
 
-        "Fig. 9. The 3D synthetic data set. We generated a three-dimensional data set which contains seven clusters, as shown in Fig. 9. Each cluster has 500 normally distributed data points. The standard deviation is 1 in each dimension. The centers of the clusters are: (0, 0, 0), (10, 0, 0), (0, 10, 0), (0, 0, 10), (-10, 0, 0), (0, -10, 0), (0, 0, -10)" - Wu.
+        "Fig. 9. The 3D synthetic data set. We generated a three-dimensional data set which contains seven clusters, as shown in Fig. 9. Each cluster has 500 normally distributed data points. The standard deviation is 1 in each dimension. The centers of the clusters are: (0, 0, 0), (10, 0, 0), (0, 10, 0), (0, 0, 10), (-10, 0, 0), (0, -10, 0), (0, 0, -10)" - Wu and Tkatsuka 2006.
     """
 
     rsName = fname%testNum+'_rs.dat'
@@ -15,10 +15,12 @@ def geo2006(fname='3d-7c-no%d', testNum=0, rand=True):
 
     cores = [(0, 0, 0), (10, 0, 0), (0, 10, 0), (0, 0, 10), (-10, 0, 0), (0, -10, 0), (0, 0, -10)]
     clusters = []
+    obsPerCore = 3571 # *7 = 24,997
+    if rand: obsPerCore = 3125 # *8 = 25,000
     for i,j,k in cores:
-        x = N.random.normal(i,1,3571)
-        y = N.random.normal(j,1,3571)
-        z = N.random.normal(k,1,3571)
+        x = N.random.normal(i,1,obsPerCore)
+        y = N.random.normal(j,1,obsPerCore)
+        z = N.random.normal(k,1,obsPerCore)
         cluster = zip(x,y,z)
         clusters.append(cluster)
     
