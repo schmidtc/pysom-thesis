e75f936038991dbb42416b424d149bade761f7ca
schmidtc <schmidtc@74a62d64-5f28-0410-90a5-c95733db7968>
2011-10-01 21:25:05 +0000

    missing files

---
+++ trunk/pysom/topology/delaunay.py
@@ -0,0 +1,45 @@
+import networkx as NX
+from math import *
+def toLngLat(xyz):
+    x,y,z = xyz
+    if z == -1 or z == 1:
+        phi = 0
+    else:
+        phi = atan2(y,x)
+        if phi == pi:
+            pass
+        elif phi > 0:
+            phi = phi-pi
+        elif phi < 0:
+            phi = phi+pi
+    theta = acos(z)-(pi/2)
+    return phi,theta
+    
+def parseDelaunay(f):
+    G = NX.Graph()
+    nodes = {}
+    edge = []
+    for line in f:
+        try:
+            line = line.strip().split()
+            line = map(float,line)
+            pt = toLngLat(line)
+            if pt not in nodes:
+                nodes[pt] = len(nodes)
+            else:
+                edge.append(nodes[pt])
+
+            if len(edge) == 2:
+                G.add_edge(edge)
+                edge = []
+        except:
+            pass
+    return G
+if __name__=="__main__":
+    import sys
+    import som
+    filename = sys.argv[1]
+    G = parseDelaunay(filename)
+
+    S = som.GraphTopology(G)
+
